066891ed7206eea12a456ced65b96ec29cbf23d3
James Wesley Goedert <jwgoedert@gmail.com>
2017-07-25 11:18:28 -0500

    (feat)Add rxjs and implement http seed/signup requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private heroesUrl = 'http://localhost:8080/v1';  // URL to web api
+  // private heroesUrl = 'https://735cb16c.ngrok.io/v1';  // URL to web api
+ constructor(public http: Http) {
+    this.http = http;
+  }
   title = 'app';
+  seedTest(){
+    return this.http
+    .get(`${this.heroesUrl}/seed`).map(info =>{
+      console.log(info);
+      return info;
+    }).subscribe(newinfo=> {
+      return newinfo;
+    })
+    
+  }
+  signUpTest(){
+    return this.http
+    .post(`${this.heroesUrl}/signup?user=jamestest4&pass=jamestest4`, Headers).map(info =>{
+      console.log(info);
+      return info;
+    }).subscribe(newinfo=> {
+      return newinfo;
+    })
+    
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MyTripsComponent } from './my-trips/my-trips.component';
+import { TripPreviewComponent } from './trip-preview/trip-preview.component';
+import { TripViewerComponent } from './trip-viewer/trip-viewer.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    DashboardComponent,
+    MyTripsComponent,
+    TripPreviewComponent,
+    TripViewerComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
